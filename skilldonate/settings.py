7b2e3c0aee8355dd2a703f70e683531692690a55
Prince D Jele <Umlamulankunzi@gmail.com>
2023-12-09 19:36:18 +0200

    modified settings for development

--- skilldonate/settings.py
+++ skilldonate/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+#for env variables
+from decouple import config
 
 from pathlib import Path
+
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(s(4uhgy3y-@@j!#xku15cnhz5l*98la!9^fx(g@#z+v&!b(i('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,9 +80,18 @@ WSGI_APPLICATION = 'skilldonate.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'old_default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'skilldonate',
+        'USER': 'umlamulankunzi',
+        'PASSWORD': 'umla_dev_578',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
