94c88900ec494420ff926881e199544457fcebc8
Prince D Jele <Umlamulankunzi@gmail.com>
2023-12-10 09:59:24 +0200

    Implemented core functionality
    Implemented core functionality, that is, about, contact, homepage. Created base template, 404 and 500 error templates

--- core/urls.py
+++ core/urls.py
@@ -6,3 +6,18 @@ from . import views
 
 urlpatterns = [
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import home, info, about, contact
+
+
+urlpatterns = [
+    path("", home, name='home'),
+    path("how-it-works", info, name='how-it-works'),
+    path("about", about, name='about'),
+    path("contact", contact, name='contact'),
+]
